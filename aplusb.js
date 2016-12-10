2e488d32da2880f130da6a6544a3258fc443742c
Maximus Meridius <max_karlsruhe@yahoo.de>
2016-12-10 21:59:15 +0100

    test2a

--- aplusb.js
+++ aplusb.js
@@ -29,30 +29,3 @@ app.get('/task2a', (req, res) => {
 app.listen(3000, function(){
   console.log('CORS-enabled web server listening on port 3000');
 });
-
-/*http.createServer(function(req,res)
-{
-  //console.log('request...');
-  var query = url.parse(req.url,true).query;
-  console.log(query);
-  console.log(new ab(null).test());
-  var abObj = new ab(JSON.parse('{"a":4,"b":3}'));
-  console.log(abObj.test());
-  res.end(JSON.stringify(query));
-*/
-
-  //res.writeHead(200, {'Content-Type' : 'text/html; charset=utf-8'});
-  
-  /*var body = '<!DOCTYPE HTML>' +
-    '<html>' + 
-      '<head>' + 
-        '<title>Test repo</title>' +
-      '</head>' +
-      '<body>' +
-        '<p>' + url.href + '</p>' +
-      '</body>' +
-    '</html>';
-  res.end(body);
-  */
-/*  console.log('----------------------------------- requested -------------------------------------------');
-}).listen(1337);*/
